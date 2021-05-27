45e7df1e1ca2b3c66ea549b175dfefe9926800d2
fewbadboy <renekton.king@qq.com>
2021-05-28 01:19:18 +0800

    20210528

--- app.js
+++ app.js
@@ -1,26 +1,38 @@
-var createError = require('http-errors');
 var express = require('express');
+var expressReactViews = require('express-react-views');
+var fs = require('fs');
 var path = require('path');
+
+var createError = require('http-errors');
+
 var cookieParser = require('cookie-parser');
+// request logger
 var logger = require('morgan');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var indexRouter = require('./routes');
 
 var app = express();
 
 // view engine setup
+var options = { beautify: true };
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.set('view engine', 'jsx');
+app.engine('jsx', expressReactViews.createEngine(options));
 
-app.use(logger('dev'));
+// log all request to STDOUT
+// app.use(logger('dev')); 
+// write logs to a file
+var accessLogStream = fs.createWriteStream(path.join(__dirname, 'dev.log'), { flags: 'a' })
+app.use(logger(':method :url :status :res[content-length] - :response-time ms', {
+  // skip: function (req, res) { return res.statusCode < 400 },
+  stream: accessLogStream
+}))
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/', indexRouter.index);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
